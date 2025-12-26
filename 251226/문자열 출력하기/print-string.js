ac4109a37117f75d338062ae8f929fa8bb53063e
LeeSlyeee <trashmechin@gmail.com>
2025-12-26 14:11:04 +0900

    Title: 문자열 출력하기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251226/문자열 출력하기/print-string.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let num = Number(input[0])
+
+for (let i = 0; i < num; i++) {
+    console.log('LeebrosCode')
+}
