d3417ee280930ca8bfdd8dfe500e544f39689c1d
LeeSlyeee <trashmechin@gmail.com>
2025-12-26 12:23:03 +0900

    Title: 물의 상태, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 251226/물의 상태/state-of-water.js
+++ 251226/물의 상태/state-of-water.js
@@ -3,9 +3,9 @@ let input = fs.readFileSync(0).toString().trim().split(' ');
 
 let temperture = Number(input[0]);
 
-if (temperture <= 0) {
+if (temperture < 0) {
     console.log('ice');
-} else if (temperture > 100) {
+} else if (temperture >= 100) {
     console.log('vapor');
 } else {
     console.log('water')
