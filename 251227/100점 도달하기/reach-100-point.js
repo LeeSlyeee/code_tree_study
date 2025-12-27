b42f6666f9233cab283a1bdd3608109a67a63124
LeeSlyeee <trashmechin@gmail.com>
2025-12-27 22:09:48 +0900

    Title: 100점 도달하기, Time: 61ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251227/100점 도달하기/reach-100-point.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let num = Number(input);
+
+const result = [];
+
+for (let i = num; i <= 100; i++) {
+    if (i >= 90) {
+        result.push('A');
+    } else if (i >= 80) {
+        result.push('B');
+    } else if (i >= 70) {
+        result.push('C');       
+    } else if (i >= 60) {
+        result.push('D');       
+    } else {
+        result.push('E');
+    }
+}
+
+console.log(result.join(' '))
