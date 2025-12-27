b4679ba62eefc06a5c075efefb5787737b7b1597
LeeSlyeee <trashmechin@gmail.com>
2025-12-27 12:05:20 +0900

    Title: 중앙값 구하기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251227/중앙값 구하기/find-the-median.js
+++ 251227/중앙값 구하기/find-the-median.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 
-let a = input[0]
-let b = input[1]
-let c = input[2]
+let a = Number(input[0])
+let b = Number(input[1])
+let c = Number(input[2])
 
 
 let median;
