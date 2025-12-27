aa7477f9c52435ebbe28b92a87f69e5c7f353844
LeeSlyeee <trashmechin@gmail.com>
2025-12-27 21:51:06 +0900

    Title: 더 긴 문자열, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251227/더 긴 문자열/longer-string.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let word1 = input[0]
+let word2 = input[1]
+
+if (word1.length > word2.length) {
+    console.log(`${word1} ${word1.length}`)
+} else if (word1.length < word2.length) {
+    console.log(`${word2} ${word2.length}`)
+} else {
+    console.log('same')
+}
