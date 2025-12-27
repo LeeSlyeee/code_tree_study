f650454414f47c9b5b270484ccc1222f8f5c941a
LeeSlyeee <trashmechin@gmail.com>
2025-12-27 22:55:34 +0900

    Title: 1부터의 합, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251227/1부터의 합/sum-from-the-one.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let num = Number(input[0])
+let tmp = 0
+
+for (let i = 0; i <= num; i++) {
+    tmp += i
+    if (tmp >= num) {
+        console.log(i);
+        break;
+    }    
+}
